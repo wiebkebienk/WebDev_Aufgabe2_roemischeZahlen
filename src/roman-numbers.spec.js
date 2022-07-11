0b8c4b1b6c7e38154bd868910e04d564c6ce2c30
Wiebke Bienk <wiebke.bienk@gmail.com>
2022-07-11 14:36:43 +0200

    Aufgabe c erledigt

--- src/roman-numbers.spec.js
+++ src/roman-numbers.spec.js
@@ -10,11 +10,25 @@ describe('romanNumbers', () => {
 //Zahlen testen
 describe('convertToRoman', () => {
     var result = 0
-    
+
     it('sollte eine I für 1 zurückgeben', () => {
         const ArabischeZahl = 1
         result = convertToRoman(ArabischeZahl)
         console.log(result);
         expect(result).toStrictEqual('I')
+    })
+
+    it('sollte eine II für 2 zurückgeben', () => {
+        const ArabischeZahl = 2
+        result = convertToRoman(ArabischeZahl)
+        console.log(result);
+        expect(result).toStrictEqual('II')
+    })
+
+    it('sollte eine III für 3 zurückgeben', () => {
+        const ArabischeZahl = 3
+        result = convertToRoman(ArabischeZahl)
+        console.log(result);
+        expect(result).toStrictEqual('III')
     })
 })
